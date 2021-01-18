a5ec8b018ebff0d56747d78e67e95905a626a9c0
ananyagraphic <75197504+ananyagraphic@users.noreply.github.com>
2021-01-18 17:51:55 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,11 @@
+class bob{
+    constructor(){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+    }
+}
+elipse(50,60,300,200)
